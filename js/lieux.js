883c33fc1846b7d6d5bc45902303c50c9b5b0360
kabomDev <kabom.y@yahoo.com>
2022-07-30 12:04:25 +0200

    escape room initialisation

---
+++ js/lieux.js
@@ -0,0 +1,99 @@
+const lieux = {
+    CHAMBRE1: {
+        id: 'CHAMBRE1',
+        name: 'chambre 1',
+        img: '/images/chambre_froide.png',
+        description: 'vous etes dans la chambre froide',
+        blocked: true,
+        blockedText: 'Il me faut la clé',
+        issues: {
+            ArrowLeft: 'CHAMBRE2',
+            ArrowRight: 'CHAMBRE3',
+        },
+        objets: [
+            'poignee',
+            'porte'
+        ]
+    },
+    CHAMBRE2: {
+        id: 'CHAMBRE2',
+        name: 'chambre 2',
+        img: '/images/chambre_nuit.png',
+        description: 'vous etes dans la chambre 2',
+        blocked: false,
+        blockedText: 'Il manque quelque chose pour ouvrir la porte',
+        issues: {
+            ArrowLeft: 'CHAMBRE3',
+            ArrowRight: 'CHAMBRE1',
+        },
+        objets: [
+            'couteau',
+            'pain'
+        ],
+        
+    },
+    CHAMBRE3: {
+        id: 'CHAMBRE3',
+        name: 'chambre 3',
+        description: 'vous etes dans la chambre 3',
+        blocked: false,
+        blockedText: '',
+        issues: {
+            ArrowLeft: 'CHAMBRE1',
+            ArrowRight: 'CHAMBRE2',
+            ArrowUp: 'CHAMBRE4',
+        }
+    },
+    CHAMBRE4: {
+        id: 'CHAMBRE4',
+        name: 'chambre 4',
+        description: 'vous etes dans la chambre 4',
+        blocked: false,
+        blockedText: '',
+        issues: {
+            ArrowDown: 'CHAMBRE3',
+        }
+    }
+}
+
+const objetsVisibles= {
+    couteau:{
+        name: 'couteau',
+        description: 'un couteau bien émoussé',
+        toujoursVisible: false,
+        inventaire: true,
+        texte: 'vous prenez le couteau',
+        combinaison: 'pain',
+        texteCombinaison: "j'avais une petite faim",
+        texteNoCombinaison: 'hhmmmm, ça va être difficile',
+    },
+    pain:{
+        name: 'pain',
+        description: 'un morceau de pain',
+        toujoursVisible: false,
+        inventaire: false,
+        texte: "Je n'ai pas faim",
+        combinaison: 'couteau',
+        texteCombinaison: "j'avais une petite faim",
+        texteNoCombinaison: 'Je ne pense pas que ça serve ici',
+    },
+    poignee:{
+        name: 'poignée',
+        description: 'une poignée de porte, cachée derriere un carton',
+        toujoursVisible: false,
+        inventaire: true,
+        texte: 'vous prenez la poignée',
+        combinaison: 'porte',
+        texteCombinaison: "la porte s'ouvre",
+        texteNoCombinaison: 'Nop',
+    },
+    porte:{
+        name: 'porte',
+        description: 'une porte en bois',
+        toujoursVisible: true,
+        inventaire: false,
+        texte: 'cette porte est beaucoup trop lourde pour moi',
+        combinaison: 'poignée',
+        texteCombinaison: "la porte s'ouvre",
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,352 @@
+const canvas = document.getElementById('canvas');
+canvas.width = 900;
+canvas.height = 600;
+const ctx = canvas.getContext('2d');
+let normal = ctx.font = '15px Roboto'
+/* ================================================ */
+/* =================VARIABLES GLOBALES======================== */
+/* ================================================ */
+
+let endroitActuel = '';
+let message = '';
+let color = '';
+
+let inventaire = [];
+let inHand = '';
+let etat = '';
+let isBlocked =false;
+
+let gauche = new Image();
+gauche.src = 'images/f-gauche.png';
+
+let droite = new Image();
+droite.src = 'images/f-droite.png';
+
+
+/* ================================================ */
+/* =================FONCTIONS======================== */
+/* ================================================ */
+
+/*======INPUT DU CLAVIER DETECTE======== */
+function seDeplacer(e)
+{
+    e.preventDefault();
+    let input = e.key;
+
+    //pour se deplacer dans les differentes pieces
+    if (lieux[endroitActuel].issues != undefined) {
+        Object.entries(lieux[endroitActuel].issues).forEach(function([key,value]){
+            if (isBlocked) {
+                message = lieux[endroitActuel].blockedText;
+            }else
+            {
+                if (input == key && lieux[value].blocked == false) {
+                    goTo(value);
+                }else if(input == key && lieux[value].blocked == true)
+                {
+                    console.log(value);
+                    message = lieux[value].blockedText;
+                }
+            }
+        })
+        removeMessage();
+    }
+
+    if (e.key == 'p') {
+        etat = 'PRENDRE';
+        return;
+    }
+
+    if (e.key == 'u') {
+        etat = 'UTILISER';
+        return;
+    }
+    
+    //si notre etat est egal a PRENDRE
+    if (etat == 'PRENDRE') {
+        message = '';
+        if (lieux[endroitActuel].objets != undefined) {
+            input -= 1;
+            Object.entries(lieux[endroitActuel].objets).forEach(function([key,value]){
+                if (input == key)
+                {
+                    //si l'objet a un inventaire a false alors on affiche le texte correspondant
+                    if (objetsVisibles[value].inventaire == false) 
+                    {
+                        message = objetsVisibles[value].texte;
+                        removeMessage();
+                    }
+                    //sinon on ajoute l'objet à l'inventaire et affiche le texte correpondant
+                    else
+                    {
+                        addToInventory(value);
+                        lieux[endroitActuel].objets.splice(key,1);
+                        message = objetsVisibles[value].texte;
+                        removeMessage();
+                    }
+                }
+                etat = '';
+            });
+        }
+    }
+
+    //si notre etat est egal a UTILISER
+    if (etat == 'UTILISER') {
+        //si notre inventaire n'est pas vide
+        if (inventaire.length != 0) {
+            //on fait -1 pour tomber sur le bon index
+            input -= 1;
+            inventaire.forEach(function(value){
+                etat = 'UTILISERSUR';
+                inHand = value;
+            });
+        }
+    }
+
+    //si notre etat est egal a UTILISERSUR
+    if (etat == 'UTILISERSUR') {
+        if (lieux[endroitActuel].objets != 0) {
+            //on fait -1 pour tomber sur le bon index
+            input -= 1;
+            Object.entries(lieux[endroitActuel].objets).forEach(function([key,value])
+            {
+                //si la touche correspond au numero
+                if (input == key)
+                {
+                    useObjectTo(inHand, value, endroitActuel);
+                }
+            });
+        }
+    }
+} 
+
+/*======fonction GO TO======== */
+function goTo(lieu)
+{
+    endroitActuel = lieu;
+}
+
+
+/*======fonction AJOUTER A INVENTAIRE======== */
+function addToInventory(obj)
+{
+    inventaire.push(obj);
+}
+
+/*======fonction RETIRER DE INVENTAIRE======== */
+function removeFromInventory(obj)
+{
+    for (let index = 0; index < inventaire.length; index++) {
+        inventaire.splice(index,1);
+        inHand = '';
+    }
+}
+
+/*======fonction UTILISER SUR UN OBJET======== */
+function useObjectTo(objInHand, objInRoom, room)
+{
+    //verifie si la combinaison entre l'objet dans notre main et celle dans la piece correspond
+    if (objetsVisibles[objInHand].combinaison === objInRoom) {
+        message = objetsVisibles[objInHand].texteCombinaison;
+        isBlocked = false;
+        lieux[room].blocked = false;
+        removeFromInventory(objInHand);
+        
+        if (objetsVisibles[objInRoom].toujoursVisible === false) {
+            delete objetsVisibles[objInRoom];
+            lieux[endroitActuel].objets.splice(objInRoom,1);
+        }
+        console.log(objetsVisibles);
+        console.log(lieux[endroitActuel].objets);
+    }
+    else{
+        message = lieux[objetsVisibles[objInRoom].texteNoCombinaison];
+    }
+
+    etat = '';
+}
+
+/* ================================================ */
+/*======fonction EFFACER LE MESSAGE =============== */
+/* ================================================ */
+function removeMessage()
+{
+    setTimeout(() => {
+        message = '';
+    }, 3000);
+}
+
+/* =================================================== */
+/*======POUR MODIFIER LA LANGUE DES DIRECTIONS======== */
+/* =================================================== */
+function translateDirection(direction)
+{
+    if (direction == 'ArrowLeft') {
+        return 'gauche';
+    }
+    if (direction == 'ArrowRight') {
+        return 'droite';
+    }
+    if (direction == 'ArrowUp') {
+        return 'haut';
+    }
+    if (direction == 'ArrowDown') {
+        return 'bas';
+    }
+}
+
+/* ====================================================== */
+/*======AFFICHE LES OBJETS VISIBLES DANS LA PIECE======== */
+/* ====================================================== */
+function visibles()
+{
+    let espace = 0;
+    
+    Object.entries(lieux[endroitActuel].objets).forEach(function([key,value]){
+        if (objetsVisibles[value] !== undefined) {
+            ctx.fillText('vous voyez :',400,450);
+            printAtWordWrap(ctx,`[ ${parseInt(key)+1} ] ${objetsVisibles[value].description}`, 400,480 + espace,20,500);
+            espace += 20;
+        }
+    });
+}
+
+/* ================================================ */
+/*======================CHARGER==================== */
+/* ================================================ */
+function load()
+{
+    //etat par defaut
+    etat = '';
+
+    //capte l'evenement des inputs
+    document.addEventListener('keydown', seDeplacer, false);
+
+    //charge le lieu de depart
+    endroitActuel = 'CHAMBRE1';
+    isBlocked = lieux[endroitActuel].blocked;
+}
+
+/*======UPDATE======== */
+function update()
+{
+    
+}
+
+/* ================================================ */
+/*======AFFICHER A L'ECRAN========================= */
+/* ================================================ */
+function draw()
+{
+    let espace = 0;
+    let piece = new Image();
+
+    //PIECES
+    if (endroitActuel == 'CHAMBRE1') {
+        piece.src = lieux.CHAMBRE1.img;
+    }else if (endroitActuel == 'CHAMBRE2') {
+        piece.src = lieux.CHAMBRE2.img;
+    }else if (endroitActuel == 'CHAMBRE3') {
+        color = 'blue';    
+    }else if (endroitActuel == 'CHAMBRE4') {
+        color = 'purple';
+    }
+
+    //dessine la pièce
+    ctx.drawImage(piece,0,0,canvas.width,400);
+
+    //message affichant dans quelle piece on est
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+    printAtWordWrap(ctx,lieux[endroitActuel].description,100,50,20,500);
+
+    //inventaire
+    //affiche le texte de notre inventaire
+    if (inventaire.length > 0) {
+        ctx.fillText('Inventaire:', 100,450);
+    }
+    
+    for (let i = 0; i < inventaire.length; i++) {
+        ctx.fillStyle = 'white';
+        printAtWordWrap(ctx,`[ ${parseInt(i)+1} ] ${inventaire[i]}`, 100,470 + espace,20,500);
+        espace += 20;
+    }
+
+    //affiche le texte de notre main
+    if (inHand.length > 0) {
+        ctx.fillText('Dans la main:', 100,550);
+    }
+
+    ctx.fillStyle = 'white';
+    printAtWordWrap(ctx,`${inHand}`, 100,550 + espace,20,500);
+    espace += 20;
+        
+
+    //affiche les issues si le lieux n'est pas bloqué
+    if (lieux[endroitActuel].issues) {
+        let direction = '';
+        if (isBlocked == false) {
+            Object.entries(lieux[endroitActuel].issues).forEach(function([key,value]){
+            
+                direction = translateDirection(key);
+    
+                printAtWordWrap(ctx,`${direction} : ${value}`, 100,100 + espace, 20, 500);
+                espace += 20;
+                
+            });
+        }
+        
+    }
+
+    //si objets visibles
+    if (lieux[endroitActuel].objets !== undefined) {
+        visibles();
+    }
+
+    //Nos actions par rapport a notre etat
+    if (etat == '' && lieux[endroitActuel].objets) {
+        if (lieux[endroitActuel].objets.length !== 0) {
+            ctx.fillStyle = 'green';
+            ctx.fillText('Commande :',400,550);
+        
+            ctx.fillText('[ P ] Prendre',400,570);
+            
+            if (inventaire.length != 0) {
+                ctx.fillText('[ U ] Utiliser',400,590)
+            }
+        }
+    }
+
+    /* ================================================ */
+    /* =================ETATS======================== */
+    /* ================================================ */ 
+
+    if (etat == 'PRENDRE'){
+        message = 'Que voulez-vous prendre ? Tapez son numéro';
+    }
+
+    if (etat == 'UTILISER') {
+        message = 'Quel objet voulez-vous utiliser ?';
+    }
+
+    if (etat == 'UTILISERSUR') {
+        message = "sur quoi voulez-vous utiliser l'objet " + inHand;
+    }
+    
+    printAtWordWrap(ctx,message, 300,370,20,500);
+}
+
+/* ================================================ */
+/* =================ANIMATE======================== */
+/* ================================================ */
+function animate()
+{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    update();
+    draw();
+
+    requestAnimationFrame(animate);
+}
+
+load();
+animate();
